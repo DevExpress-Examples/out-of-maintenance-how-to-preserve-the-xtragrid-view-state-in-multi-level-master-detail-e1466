8ba376d2c3e16867ab4085b5c60caa21c7c57b8f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to preserve the XtraGrid view state in multi level master/detail
+
+
+<p>This example is an extension of example <a href="https://www.devexpress.com/Support/Center/p/E776">How to preserve the XtraGrid View state</a> for a grid in master-detail mode. It stores view states of all existing nested grid views.</p>
+
+
+<h3>Description</h3>
+
+<p>2010-09-28 Michael: Modified to directly work with data rows. Now, it is not required to create a grid column for the key field.<br /> 2010-09-29 Michael: Fixed focused group row bug. See issue ID: Q276278.</p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,179 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports System.Data
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Helpers
+
+
+Namespace RefreshHelperApp
+	''' <summary>
+	''' Summary description for Form1.
+	''' </summary>
+	Public Class Form1
+		Inherits System.Windows.Forms.Form
+		Private gridControl1 As DevExpress.XtraGrid.GridControl
+		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+		Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
+		Private panel1 As System.Windows.Forms.Panel
+		Private WithEvents checkEdit1 As DevExpress.XtraEditors.CheckEdit
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.Container = Nothing
+
+		Public Sub New()
+			'
+			' Required for Windows Form Designer support
+			'
+			InitializeComponent()
+
+			'
+			' TODO: Add any constructor code after InitializeComponent call
+			'
+		End Sub
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		Protected Overrides Overloads Sub Dispose(ByVal disposing As Boolean)
+			If disposing Then
+				If components IsNot Nothing Then
+					components.Dispose()
+				End If
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+			Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+			Me.panel1 = New System.Windows.Forms.Panel()
+			Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.panel1.SuspendLayout()
+			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' gridControl1
+			' 
+			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.gridControl1.EmbeddedNavigator.Name = ""
+			Me.gridControl1.Location = New System.Drawing.Point(0, 40)
+			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Name = "gridControl1"
+			Me.gridControl1.Size = New System.Drawing.Size(816, 510)
+			Me.gridControl1.TabIndex = 0
+			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+			' 
+			' gridView1
+			' 
+			Me.gridView1.GridControl = Me.gridControl1
+			Me.gridView1.Name = "gridView1"
+			Me.gridView1.OptionsSelection.MultiSelect = True
+			' 
+			' simpleButton1
+			' 
+			Me.simpleButton1.Location = New System.Drawing.Point(8, 8)
+			Me.simpleButton1.Name = "simpleButton1"
+			Me.simpleButton1.Size = New System.Drawing.Size(75, 23)
+			Me.simpleButton1.TabIndex = 1
+			Me.simpleButton1.Text = "Save"
+'			Me.simpleButton1.Click += New System.EventHandler(Me.simpleButton1_Click);
+			' 
+			' simpleButton2
+			' 
+			Me.simpleButton2.Location = New System.Drawing.Point(104, 8)
+			Me.simpleButton2.Name = "simpleButton2"
+			Me.simpleButton2.Size = New System.Drawing.Size(72, 24)
+			Me.simpleButton2.TabIndex = 2
+			Me.simpleButton2.Text = "Load"
+'			Me.simpleButton2.Click += New System.EventHandler(Me.simpleButton2_Click);
+			' 
+			' panel1
+			' 
+			Me.panel1.Controls.Add(Me.checkEdit1)
+			Me.panel1.Controls.Add(Me.simpleButton1)
+			Me.panel1.Controls.Add(Me.simpleButton2)
+			Me.panel1.Dock = System.Windows.Forms.DockStyle.Top
+			Me.panel1.Location = New System.Drawing.Point(0, 0)
+			Me.panel1.Name = "panel1"
+			Me.panel1.Size = New System.Drawing.Size(816, 40)
+			Me.panel1.TabIndex = 3
+			' 
+			' checkEdit1
+			' 
+			Me.checkEdit1.Location = New System.Drawing.Point(208, 12)
+			Me.checkEdit1.Name = "checkEdit1"
+			Me.checkEdit1.Properties.Caption = "Cell selection"
+			Me.checkEdit1.Size = New System.Drawing.Size(123, 19)
+			Me.checkEdit1.TabIndex = 3
+'			Me.checkEdit1.CheckedChanged += New System.EventHandler(Me.checkEdit1_CheckedChanged);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+			Me.ClientSize = New System.Drawing.Size(816, 550)
+			Me.Controls.Add(Me.gridControl1)
+			Me.Controls.Add(Me.panel1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.panel1.ResumeLayout(False)
+			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+		#End Region
+
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread> _
+		Shared Sub Main()
+			Application.Run(New Form1())
+		End Sub
+
+		Private helper As GridControlState
+
+		Private ds As DataSet
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+			ds = New DataSet()
+			ds.ReadXml("nwind.xml")
+			gridView1.OptionsView.ColumnAutoWidth = False
+			gridControl1.DataSource = ds.Tables("Categories")
+
+			helper = New GridControlState(New GridControlState.ViewDescriptor(){ New GridControlState.ViewDescriptor("", "CategoryID"), New GridControlState.ViewDescriptor("CategoriesProducts", "ProductID"), New GridControlState.ViewDescriptor("ProductsOrderDetails", "OrderID")})
+		End Sub
+
+		Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles simpleButton1.Click
+			helper.SaveViewInfo(gridView1)
+		End Sub
+
+		Private Sub simpleButton2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles simpleButton2.Click
+			helper.LoadViewInfo(gridView1)
+		End Sub
+
+		Private Sub checkEdit1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles checkEdit1.CheckedChanged
+			If (CType(sender, DevExpress.XtraEditors.CheckEdit)).Checked Then
+				gridView1.OptionsSelection.MultiSelectMode = GridMultiSelectMode.CellSelect
+			Else
+				gridView1.OptionsSelection.MultiSelectMode = GridMultiSelectMode.RowSelect
+			End If
+		End Sub
+	End Class
+End Namespace
